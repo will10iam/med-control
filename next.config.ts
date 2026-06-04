37b8b30f15a9c46055641716cf37887382b2ccc2
William <you@example.com>
2026-06-04 11:19:20 -0300

    fix: ajustando para PWA + Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
